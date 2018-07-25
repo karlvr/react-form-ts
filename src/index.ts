f734011f2bd8f13bfaad9df546377cd1d9b17695
Karl von Randow <karl@xk72.com>
2018-07-26 09:14:57 +1200

    Add typings and an index

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { SimpleFormState as SimpleFormStateClass } from './SimpleFormState'
+import { PatchFormState as PatchFormStateClass } from './PatchFormState'
+
+export const SimpleFormState = SimpleFormStateClass
+export const PatchFormState = PatchFormStateClass
