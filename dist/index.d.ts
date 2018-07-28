7d9339306e7f455b9630e7cfbe40978cfe044b0d
Karl von Randow <karl@xk72.com>
2018-07-29 08:44:46 +1200

    Remove dist from git
    We’ll just have to use link or publish this!

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { FormState } from './FormState';
-export { SimpleFormState } from './SimpleFormState';
-export { PatchFormState } from './PatchFormState';
-export { default as Input } from './components/Input';
-export { default as Select } from './components/Select';
-export { default as Scope } from './components/Scope';
--- dist/types.d.ts
+++
@@ -1,6 +0,0 @@
-export declare type ArrayProperties<T> = {
-    [P in keyof T]: T[P] extends (infer R)[] ? R : never;
-};
-export declare type Combined<SOURCE, PATCH> = {
-    [P in keyof SOURCE & keyof PATCH]: SOURCE[P] & PATCH[P];
-};
