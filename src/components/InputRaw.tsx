4c8fa07db6c29a00051b413442e6e2f60fa2b6a6
Karl von Randow <karl@xk72.com>
2018-08-05 11:13:44 +1200

    InputRaw and SelectRaw even more basic

--- src/components/InputRaw.tsx
+++ src/components/InputRaw.tsx
@@ -4,30 +4,26 @@ import { FormState } from '../FormState'
 type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
 type ElementAttributes = React.InputHTMLAttributes<HTMLInputElement>
 
-interface OwnProps<FORM> extends Omit<ElementAttributes, 'name' | 'value' | 'onChange'> {
-	formState: FormState<FORM>
-	onNewFormState: (newState: FormState<FORM>, name: keyof FORM) => void
-	toValue: (formState: FormState<FORM>) => ElementValueType
-	fromValue: (value: ElementValueType, formState: FormState<FORM>) => FormState<FORM>
+interface OwnProps extends Omit<ElementAttributes, 'value' | 'onChange'> {
+	value: ElementValueType
+	onValue: (name: string, value: ElementValueType) => void
 }
 
 type ElementValueType = ElementAttributes['value']
 
-export default class InputRaw<FORM> extends React.Component<OwnProps<FORM>> {
+export default class InputRaw<FORM> extends React.Component<OwnProps> {
 
 	onChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
-		const name = evt.target.name as {} as keyof FORM
+		const name = evt.target.name
 		const elementValue = evt.target.value
-		const newState = this.props.fromValue(elementValue, this.props.formState)
-		this.props.onNewFormState(newState, name)
+		this.props.onValue(name, elementValue)
 	}
 
 	render() {
-		const { formState, onNewFormState, toValue, fromValue, ...rest } = this.props
-		const value = this.props.toValue(formState)
+		const { value, onValue, ...rest } = this.props
 
 		return (
-			<input name={name} onChange={this.onChange} value={value || ''} {...rest} />
+			<input onChange={this.onChange} value={value || ''} {...rest} />
 		)
 	}
 }
--- src/components/SelectRaw.tsx
+++ src/components/SelectRaw.tsx
@@ -4,30 +4,26 @@ import { FormState } from '../FormState'
 type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
 type ElementAttributes = React.InputHTMLAttributes<HTMLSelectElement>
 
-interface OwnProps<FORM> extends Omit<ElementAttributes, 'name' | 'value' | 'onChange'> {
-	formState: FormState<FORM>
-	onNewFormState: (newState: FormState<FORM>, name: keyof FORM) => void
-	toValue: (formState: FormState<FORM>) => ElementValueType
-	fromValue: (value: ElementValueType, formState: FormState<FORM>) => FormState<FORM>
+interface OwnProps extends Omit<ElementAttributes, 'value' | 'onChange'> {
+	value: ElementValueType
+	onValue: (name: string, value: ElementValueType) => void
 }
 
 type ElementValueType = ElementAttributes['value']
 
-export default class SelectRaw<FORM> extends React.Component<OwnProps<FORM>> {
+export default class SelectRaw<FORM> extends React.Component<OwnProps> {
 
 	onChange = (evt: React.ChangeEvent<HTMLSelectElement>) => {
-		const name = evt.target.name as {} as keyof FORM
+		const name = evt.target.name
 		const elementValue = evt.target.options[evt.target.selectedIndex].value
-		const newState = this.props.fromValue(elementValue, this.props.formState)
-		this.props.onNewFormState(newState, name)
+		this.props.onValue(name, elementValue)
 	}
 
 	render() {
-		const { formState, onNewFormState, toValue, fromValue, children, ...rest } = this.props
-		const value = this.props.toValue(formState)
+		const { value, onValue, children, ...rest } = this.props
 
 		return (
-			<select name={name} onChange={this.onChange} value={value || ''} {...rest}>
+			<select onChange={this.onChange} value={value || ''} {...rest}>
 				{children}
 			</select>
 		)
