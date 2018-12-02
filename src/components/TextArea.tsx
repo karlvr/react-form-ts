ed00978e19bf37dc6ecd7002bd944eae4b359d72
Karl von Randow <karl@xk72.com>
2018-12-02 21:24:20 +1300

    TextArea

---
+++ src/components/TextArea.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react'
+import { FormState } from '../FormState'
+
+type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
+type ElementAttributes = React.TextareaHTMLAttributes<HTMLTextAreaElement>
+
+interface OwnProps<FORM, K extends keyof FORM> extends Omit<ElementAttributes, 'name' | 'value' | 'onChange'> {
+	name: K & string
+	formState: FormState<FORM>
+	onNewFormState: (newState: FormState<FORM>, name: K) => void
+	onValue?: (name: K, value: ElementValueType) => FormState<FORM> | undefined
+}
+
+type ElementValueType = ElementAttributes['value']
+
+export default class TextArea<FORM, K extends keyof FORM> extends React.Component<OwnProps<FORM, K>> {
+
+	onChange = (evt: React.ChangeEvent<HTMLTextAreaElement>) => {
+		const name = evt.target.name as {} as K
+		const elementValue = evt.target.value
+
+		if (this.props.onValue) {
+			const newFormState = this.props.onValue(name, elementValue)
+			if (newFormState) {
+				this.props.onNewFormState(newFormState, name)
+			}
+		} else {
+			const value = elementValue as {} as FORM[keyof FORM]
+			this.props.onNewFormState(this.props.formState.set(name, value), name)
+		}
+	}
+
+	render() {
+		const { name, formState, onNewFormState, onValue, ...rest } = this.props
+		const value = formState.get(name) as {} as ElementValueType
+
+		return (
+			<textarea name={name} onChange={this.onChange} value={value || ''} {...rest} />
+		)
+	}
+}
